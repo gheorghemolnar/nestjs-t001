0b9657b0a09637265e47dba3e63089475394da79
gheorghe.molnar <molnargheorghe@gmail.com>
2022-04-21 20:00:47 +0200

    Front: App update by adding Url

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,22 +4,41 @@ import {
   useAddMessageMutation,
   useGetMessagesQuery,
 } from './graphql/message.generated';
+import {
+  useGetUrlsQuery,
+  useAddUrlMutation,
+} from './graphql/url.generated';
 
 function App() {
   const [newMessage, setNewMessage] = useState({ value: '' });
   const { data, refetch } = useGetMessagesQuery();
   const [addMessage] = useAddMessageMutation();
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onChangeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewMessage({ value: e.target.value });
   };
-
-  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const onSubmitMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (newMessage.value) {
       await addMessage({ variables: { message: newMessage.value } });
       setNewMessage({ value: '' });
       await refetch();
+    }
+  };
+
+  const [newUrl, setNewUrl] = useState({ value: '' });
+  const { data: dataUrls, refetch: refetchUrls } = useGetUrlsQuery();
+  const [addUrl] = useAddUrlMutation();
+
+  const onChangeUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewUrl({ value: e.target.value });
+  };
+  const onSubmitUrl = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newUrl.value) {
+      await addUrl({ variables: { url: newUrl.value } });
+      setNewUrl({ value: '' });
+      await refetchUrls();
     }
   };
 
@@ -47,14 +66,14 @@ function App() {
           <div className="font-semibold">
             <form
               className="flex gap-4"
-              onSubmit={onSubmit}
+              onSubmit={onSubmitMessage}
             >
               <input
                 data-cy="messageInput"
                 placeholder="Your message"
                 className="p-3 w-96 border-2 rounded-full border-main-blue"
                 value={newMessage.value}
-                onChange={onChange}
+                onChange={onChangeMessage}
               />
               <button
                 data-cy="submit"
@@ -62,6 +81,40 @@ function App() {
                 className="p-3 bg-main-blue text-white rounded-full"
               >
                 Add message
+              </button>
+            </form>
+          </div>
+        </div>
+        <div
+          data-cy="urleContainer"
+          className="p-8 flex flex-col gap-6 items-center bg-white rounded-2xl"
+        >
+          <div
+            className="font-semibold text-xl"
+          >
+            Add a few urls to ensure that everything is working correctly :
+          </div>
+          {dataUrls?.urls.map((url) => (
+            <div key={url.id}>{`${url.url_orig} ----- ${url.url_new}`}</div>
+          ))}
+          <div className="font-semibold">
+            <form
+              className="flex gap-4"
+              onSubmit={onSubmitUrl}
+            >
+              <input
+                data-cy="urlInput"
+                placeholder="Your url"
+                className="p-3 w-96 border-2 rounded-full border-main-blue"
+                value={newUrl.value}
+                onChange={onChangeUrl}
+              />
+              <button
+                data-cy="submit"
+                type="submit"
+                className="p-3 bg-main-blue text-white rounded-full"
+              >
+                Add url
               </button>
             </form>
           </div>
